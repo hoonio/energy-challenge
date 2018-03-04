865c4e89e4b8a316626c064dae36deb76a7dea4f
Hoonio <hoonio@hoonio.com>
2018-03-04 03:40:50 +0000

    average actual meter usage

--- dataprocessor/src/index.js
+++ dataprocessor/src/index.js
@@ -2,16 +2,6 @@ const METER_DIGIT = 3;
 
 function calculateEnergyUsage(meterReadings) {
   // throw new Error('Not implemented');
-  /*
-     These tests should help you getting started.
-     Input: each meter reading contains 3 properties:
-      * cumulative: the meter reading number in kWh
-      * quality: whether it is an Estimated or Manual reading. This is for information only
-      * readingDate: the date at which the meter reading was provided.
-    Output: at a minimum it should contain the energy usage and the period
-      * energyUsage: the energy usage over the calculated period
-      * from and to: date range the energy usage is calculated on
-  */
   if (meterReadings.length < 2)
     return [];
   var calculatedEnergyUsage = [];
@@ -21,12 +11,30 @@ function calculateEnergyUsage(meterReadings) {
   for (var i=1; i<meterReadings.length; i++) {
     var thisReading = meterReadings[i].cumulative;
     var currentUsage = thisReading - lastReading;
-    if (isNaN(thisReading) || (i+1<meterReadings.length && thisReading > meterReadings[i+1].cumulative)) {
-      for (var j=i+1; j<meterReadings.length; j++) {
-        if (!isNaN(meterReadings[j].cumulative)){
-          currentUsage = (meterReadings[j].cumulative - lastReading)/(j-i+1);
+    if (isNaN(thisReading)) {
+      currentUsage = fixBadReading(meterReadings, i, lastReading);
+      // for (var j=i+1; j<meterReadings.length; j++) {
+      //   if (!isNaN(meterReadings[j].cumulative)){
+      //     currentUsage = (meterReadings[j].cumulative - lastReading)/(j-i+1);
+      //     break;
+      //   }
+      // }
+    }
+    else if (thisReading < lastReading) {
+      var periodFixCount = 1;
+      for (var j=i-1; j>=0; j--){
+        if (meterReadings[j].cumulative > thisReading && meterReadings[j].quality == "Estimated"){
+          periodFixCount++;
+        }
+        else {
+          lastReading = meterReadings[j].cumulative;
           break;
         }
+      }
+      currentUsage = (thisReading - lastReading)/periodFixCount;
+      for (var j=i-periodFixCount; j<i-1; j++){
+        calculatedEnergyUsage[j].energyUsage = currentUsage;
+        lastReading += currentUsage;
       }
     }
     calculatedEnergyUsage.push({
@@ -36,9 +44,21 @@ function calculateEnergyUsage(meterReadings) {
     })
     lastReading += currentUsage;
   }
-
+  console.log(calculatedEnergyUsage)
   return calculatedEnergyUsage;
 }
 
+function fixBadReading(meterReadings, sequence, lastReading) {
+  for (var j=sequence+1; j<meterReadings.length; j++) {
+    if (!isNaN(meterReadings[j].cumulative)){
+      return (meterReadings[j].cumulative - lastReading)/(j-sequence+1);
+    }
+  }
+}
+
+function fixDescendReading(){
+  // fix for decreasing meter reading should execute retrospectively unless the loop runs in reverse...
+
+}
 
 module.exports = calculateEnergyUsage;
